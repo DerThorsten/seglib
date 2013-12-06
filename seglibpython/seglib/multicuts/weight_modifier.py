05300e080aa8b79460f0af33969ccfd01731fefa
DerThorsten <thorsten.beier@iwr.uni-heidelberg.de>
2013-12-06 10:54:31 +0100

    pls let me have commited everything =)

---
+++ seglibpython/seglib/multicuts/weight_modifier.py
@@ -0,0 +1,20 @@
+
+
+
+
+
+######################################
+# general weight sampling
+######################################
+
+
+def gaussPertubation(weight,weightStt,offsetStd=0.0,out=None,n=1):
+	"""" very stupid sampling"""
+	pass
+
+
+
+
+######################################
+# general weight sampling
+######################################
